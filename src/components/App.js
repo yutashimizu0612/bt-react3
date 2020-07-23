b4d316a4c3edb993c5442962a5c2b820bdc3c1da
yutashimizu0612 <s08388yskh@gmail.com>
2020-07-23 13:17:54 +0900

    BrowserRouterを省略

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import Register from '../pages/Register';
 import Login from '../pages/Login';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Route exact path="/" component={Register}></Route>
       <Route exact path="/login" component={Login}></Route>
-    </BrowserRouter>
+    </Router>
   );
 }
 
