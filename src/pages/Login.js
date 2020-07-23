68aa29224bf32e738701355ae4c57c92d335a3bf
yutashimizu0612 <s08388yskh@gmail.com>
2020-07-23 20:02:01 +0900

    ログイン画面作成

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,49 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
   return (
-    <div>
-      <h2>ログイン画面</h2>
-    </div>
+    <>
+      <h2 className="title is-3 mb-6 has-text-centered">ログイン画面</h2>
+      <div className="field is-horizontal">
+        <div className="field-label is-normal">
+          <label className="label">メールアドレス</label>
+        </div>
+        <div className="field-body">
+          <div className="field">
+            <p className="control">
+              <input
+                className="input"
+                type="email"
+                placeholder="メールアドレス"
+              />
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="field is-horizontal">
+        <div className="field-label is-normal">
+          <label className="label">パスワード</label>
+        </div>
+        <div className="field-body">
+          <div className="field">
+            <p className="control">
+              <input
+                className="input"
+                type="password"
+                placeholder="パスワード"
+              />
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="mt-6 has-text-centered">
+        <button className="button">ログイン</button>
+      </div>
+      <div className="mt-2 has-text-centered">
+        <Link to="/">新規登録はこちらから</Link>
+      </div>
+    </>
   );
 };
 
