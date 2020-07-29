7a76c43ffb7b64bee49fafd7e6a65e352aee311d
yutashimizu0612 <s08388yskh@gmail.com>
2020-07-29 09:45:30 +0900

    3項目全て入力するまではボタンをdisabledにする

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,95 +1,116 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import firebase from '../firebase';
 
-const Register = () => {
-  const registerUser = e => {
-    e.preventDefault();
-    const userName = e.target.user_name.value;
-    const email = e.target.email.value;
-    const password = e.target.password.value;
+export default class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      password: '',
+    };
+    this.isFormValid = this.isFormValid.bind(this);
+  }
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  isFormValid = () => {
+    const { name, email, password } = this.state;
+    return name && email && password;
+  };
+  registerUser = e => {
     // firebaseへの新規ユーザ登録処理
     firebase
       .auth()
-      .createUserWithEmailAndPassword(email, password)
+      .createUserWithEmailAndPassword(this.state.email, this.state.password)
       .then(function () {
         const user = firebase.auth().currentUser;
         user.updateProfile({
-          displayName: userName,
+          displayName: this.state.name,
         });
       })
       .catch(function (error) {
         console.log(error);
       });
     // フォームの値を空にする
-    e.target.user_name.value = '';
+    e.target.name.value = '';
     e.target.email.value = '';
     e.target.password.value = '';
   };
-  return (
-    <>
-      <h2 className="title is-3 mb-6 has-text-centered">新規登録画面</h2>
-      <form onSubmit={e => registerUser(e)}>
-        <div className="field is-horizontal">
-          <div className="field-label is-normal">
-            <label className="label">ユーザ名</label>
-          </div>
-          <div className="field-body">
-            <div className="field">
-              <p className="control">
-                <input
-                  className="input"
-                  type="text"
-                  name="user_name"
-                  placeholder="ユーザ名"
-                />
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="field is-horizontal">
-          <div className="field-label is-normal">
-            <label className="label">メールアドレス</label>
-          </div>
-          <div className="field-body">
-            <div className="field">
-              <p className="control">
-                <input
-                  className="input"
-                  type="email"
-                  name="email"
-                  placeholder="メールアドレス"
-                />
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="field is-horizontal">
-          <div className="field-label is-normal">
-            <label className="label">パスワード</label>
-          </div>
-          <div className="field-body">
-            <div className="field">
-              <p className="control">
-                <input
-                  className="input"
-                  type="password"
-                  name="password"
-                  placeholder="パスワード"
-                />
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="mt-6 has-text-centered">
-          <input className="button" type="submit" value="新規登録" />
-        </div>
-      </form>
-      <div className="mt-2 has-text-centered">
-        <Link to="/login">ログインはこちらから</Link>
-      </div>
-    </>
-  );
-};
 
-export default Register;
+  render() {
+    return (
+      <>
+        <h2 className="title is-3 mb-6 has-text-centered">新規登録画面</h2>
+        <form onSubmit={e => this.registerUser(e)}>
+          <div className="field is-horizontal">
+            <div className="field-label is-normal">
+              <label className="label">ユーザ名</label>
+            </div>
+            <div className="field-body">
+              <div className="field">
+                <p className="control">
+                  <input
+                    className="input"
+                    type="text"
+                    name="name"
+                    placeholder="ユーザ名"
+                    onChange={e => this.handleChange(e)}
+                  />
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="field is-horizontal">
+            <div className="field-label is-normal">
+              <label className="label">メールアドレス</label>
+            </div>
+            <div className="field-body">
+              <div className="field">
+                <p className="control">
+                  <input
+                    className="input"
+                    type="email"
+                    name="email"
+                    placeholder="メールアドレス"
+                    onChange={e => this.handleChange(e)}
+                  />
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="field is-horizontal">
+            <div className="field-label is-normal">
+              <label className="label">パスワード</label>
+            </div>
+            <div className="field-body">
+              <div className="field">
+                <p className="control">
+                  <input
+                    className="input"
+                    type="password"
+                    name="password"
+                    placeholder="パスワード"
+                    onChange={e => this.handleChange(e)}
+                  />
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="mt-6 has-text-centered">
+            <input
+              className="button"
+              type="submit"
+              value="新規登録"
+              disabled={!this.isFormValid()}
+            />
+          </div>
+        </form>
+        <div className="mt-2 has-text-centered">
+          <Link to="/login">ログインはこちらから</Link>
+        </div>
+      </>
+    );
+  }
+}
