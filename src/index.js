617f6aa6e26965ea57c7e6d44690f37604fbcde1
yutashimizu0612 <s08388yskh@gmail.com>
2020-08-04 06:44:36 +0900

    Redux devtoolの設定

--- src/index.js
+++ src/index.js
@@ -10,7 +10,10 @@ import 'bulma/css/bulma.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <Provider store={store}>
