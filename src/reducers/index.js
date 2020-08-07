8d78ae3751ed565e44e3e344b556618c262bc4cb
yutashimizu0612 <s08388yskh@gmail.com>
2020-08-07 09:26:36 +0900

    react-redux-firebaseインストール / firebaseReducerの導入

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
 import auth from './auth';
 import user from './user';
+import { firebaseReducer } from 'react-redux-firebase';
 
-export default combineReducers({ auth, user });
+export default combineReducers({ auth, user, firebase: firebaseReducer });
