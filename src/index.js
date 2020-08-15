eeb0da2fd449c940797162339d5d5f338dce7cb3
yutashimizu0612 <s08388yskh@gmail.com>
2020-08-15 10:42:33 +0900

    ローディング画面作成

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
+import { Provider, useSelector } from 'react-redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import firebase, { FIREBASE_CONFIG } from './firebase';
-import { ReactReduxFirebaseProvider, getFirebase } from 'react-redux-firebase';
+import {
+  ReactReduxFirebaseProvider,
+  getFirebase,
+  isLoaded,
+} from 'react-redux-firebase';
 import reducer from './reducers';
 
 import './index.css';
 import 'bulma/css/bulma.css';
 
 import App from './components/App';
+import Loading from './components/Loading';
 import * as serviceWorker from './serviceWorker';
 
 const store = createStore(
@@ -25,10 +30,18 @@ const rrfProps = {
   dispatch: store.dispatch,
 };
 
+function AuthIsLoaded({ children }) {
+  const auth = useSelector(state => state.firebase.auth);
+  if (!isLoaded(auth)) return <Loading />;
+  return children;
+}
+
 ReactDOM.render(
   <Provider store={store}>
     <ReactReduxFirebaseProvider {...rrfProps}>
-      <App />
+      <AuthIsLoaded>
+        <App />
+      </AuthIsLoaded>
     </ReactReduxFirebaseProvider>
   </Provider>,
   document.getElementById('root')
