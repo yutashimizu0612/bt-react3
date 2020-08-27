cdefcc0cbc3dc32b7c411a0338437c28a30afcaa
yutashimizu0612 <s08388yskh@gmail.com>
2020-08-28 08:18:26 +0900

    Reduxのuserが不要のため削除

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
 import auth from './auth';
-import user from './user';
 import { firebaseReducer } from 'react-redux-firebase';
 import { firestoreReducer } from 'redux-firestore';
 
 export default combineReducers({
   auth,
-  user,
   firebase: firebaseReducer,
   firestore: firestoreReducer,
 });
