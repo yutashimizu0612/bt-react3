a0e66e3a432ff19c774145ef154e70a143470858
yutashimizu0612 <s08388yskh@gmail.com>
2020-08-29 19:49:57 +0900

    Material-UIのmodalを設置

--- src/components/WalletModal.js
+++ src/components/WalletModal.js
@@ -1,31 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { closeWalletModal } from '../actions/modal';
+import Modal from '@material-ui/core/Modal';
 
 const WalletModal = props => {
+  const { closeWalletModal } = props;
   const { name, possession, isOpen } = props.modal;
   return (
-    isOpen && (
-      <div className="modal">
-        <div className="modal-background"></div>
-        <div className="modal-card">
-          <header className="modal-card-head">
-            <p className="modal-card-title">{name}さんの残高</p>
-            <button
-              className="delete"
-              aria-label="close"
-              onClick={() => closeWalletModal()}></button>
-          </header>
-          <section className="modal-card-body">{possession}</section>
-          <footer className="modal-card-foot">
-            <button className="button" onClick={() => closeWalletModal()}>
-              Close
-            </button>
-          </footer>
-        </div>
-        <button className="modal-close is-large" aria-label="close"></button>
+    <Modal open={isOpen} onClose={closeWalletModal}>
+      <div className="modal-card">
+        <header className="modal-card-head">
+          <p className="modal-card-title">{name}さんの残高</p>
+        </header>
+        <section className="modal-card-body">{possession}</section>
       </div>
-    )
+    </Modal>
   );
 };
 
