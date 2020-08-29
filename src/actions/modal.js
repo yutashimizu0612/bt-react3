d88bc11d625928302a382598c042dd76d2460c97
yutashimizu0612 <s08388yskh@gmail.com>
2020-08-29 21:39:06 +0900

    openWalletModalをもっと簡単に書く

--- src/actions/modal.js
+++ src/actions/modal.js
@@ -2,9 +2,7 @@ export const OPEN_WALLET_MODAL = 'OPEN_WALLET_MODAL';
 export const CLOSE_WALLET_MODAL = 'CLOSE_WALLET_MODAL';
 
 export const openWalletModal = (name, possession) => {
-  return dispatch => {
-    dispatch({ type: OPEN_WALLET_MODAL, name, possession });
-  };
+  return { type: OPEN_WALLET_MODAL, name, possession };
 };
 
 export const closeWalletModal = () => {
